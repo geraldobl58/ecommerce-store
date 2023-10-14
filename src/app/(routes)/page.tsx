a2f508eea45f7c0aaf671999319ad2e199c8abb9
Geraldo Luiz <geraldobl58@gmail.com>
2023-10-14 11:23:54 -0300

    feat: Add page home

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,5 +1,26 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("8ab05718-d28a-4b75-8138-b22428a4961d");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produtos em Destaques" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
