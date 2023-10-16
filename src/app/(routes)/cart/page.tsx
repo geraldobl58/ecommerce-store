20e387d63f6d9aa4d6a88db625ed225d0d952245
Geraldo Luiz <geraldobl58@gmail.com>
2023-10-16 19:45:01 -0300

    feat: Add cart page

---
+++ src/app/(routes)/cart/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Container from "@/components/ui/container";
+import CartItem from "./components/cart-item";
+import Summary from "./components/summary";
+
+import useCart from "@/hooks/use-cart";
+
+const CartPage = () => {
+  const cart = useCart();
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-black">Carrinho de compras</h1>
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            <div className="lg:col-span-7">
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">
+                  Não existe produto(s) no carrinho.
+                </p>
+              )}
+              <ul>
+                {cart.items.map((item) => (
+                  <CartItem key={item.id} data={item} />
+                ))}
+              </ul>
+            </div>
+            <Summary />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default CartPage;
